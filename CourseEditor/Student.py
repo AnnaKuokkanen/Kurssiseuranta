1c80958a08378c0a8d481b1cc8ca0e59cbae5795
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-05-14 02:26:59 +0300

    Luokka opiskelijalle

--- CourseEditor/Student.py
+++ CourseEditor/Student.py
@@ -1,1 +1,4 @@
-print('Student')
+class Student:
+  def __init__(self, id, name):
+    self.id = id
+    self.name = name
--- CourseEditor/hello.py
+++
@@ -1,4 +0,0 @@
-Python 3.6.9 (default, Apr 18 2020, 01:56:04) 
-[GCC 8.4.0] on linux
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print('Hello World!')
