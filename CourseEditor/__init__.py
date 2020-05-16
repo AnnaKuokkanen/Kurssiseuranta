c2cb138a9e18dcc5974953556f661e21454cf90f
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-05-16 04:52:47 +0300

    Otetaan käyttöön tietokanta

--- CourseEditor/__init__.py
+++ CourseEditor/__init__.py
@@ -1,3 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
+
+from flask_sqlalchemy import SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///courses.db"
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
+
 from CourseEditor import views
+
+db.create_all()
