0fd5ddb450b41ca2e60efd81ab111f606e5a492e
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-05-16 06:42:44 +0300

    html-sivujen päivitys

--- CourseEditor/views.py
+++ CourseEditor/views.py
@@ -5,6 +5,10 @@ from CourseEditor import app
 def index():
     return render_template("index.html")
 
+@app.route("/menu.html")
+def menu():
+    return render_template("menu.html")
+
 @app.route("/plan.html")
 def plan():
     return render_template("plan.html")
