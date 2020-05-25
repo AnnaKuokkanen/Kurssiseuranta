d7bb6dd76ab40302a1c4d97d0c14a54a7afc9b4d
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-05-25 03:46:28 +0300

    __init__.py-tiedostossa järjestetty importeja aakkosjärjestykseen

--- CourseEditor/__init__.py
+++ CourseEditor/__init__.py
@@ -9,10 +9,10 @@ app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
 from CourseEditor import views
-from CourseEditor.users import models
-from CourseEditor.users import views
-from CourseEditor.plans import views
 from CourseEditor.courses import models
 from CourseEditor.courses import views
+from CourseEditor.plans import views
+from CourseEditor.users import models
+from CourseEditor.users import views
 
 db.create_all()
