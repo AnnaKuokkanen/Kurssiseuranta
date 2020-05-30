62ffbc40f5241804471c593eb209b9d1899ed6e9
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-05-31 02:11:30 +0300

    Uusi tietokantataulu opettajille

--- CourseEditor/courses/models.py
+++ CourseEditor/courses/models.py
@@ -9,6 +9,8 @@ class Course(db.Model):
     content = db.Column(db.String(144), nullable=False)
     time = db.Column(db.String(144), nullable=False)
 
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'), nullable=False)
+
     accounts = db.relationship("User", secondary='account_course', backref='course', lazy=True)
 
     def __init__(self, name, content, time):
