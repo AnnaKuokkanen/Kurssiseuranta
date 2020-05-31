0ecde99e1ad37279962feb08f1bdb96cf00b40ec
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-05-31 13:25:58 +0300

    Lisää attribuutteja kurssille.

--- CourseEditor/courses/models.py
+++ CourseEditor/courses/models.py
@@ -8,14 +8,16 @@ class Course(db.Model):
     name = db.Column(db.String(144), nullable=False)
     content = db.Column(db.String(144), nullable=False)
     time = db.Column(db.String(144), nullable=False)
-    planed = db.Column(db.Boolean, nullable=False)
+    planned = db.Column(db.Boolean, nullable=False)
     completed = db.Column(db.Boolean, nullable=False)
 
     teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'), nullable=False)
 
     accounts = db.relationship("User", secondary='account_course', backref='course', lazy=True)
 
-    def __init__(self, name, content, time):
+    def __init__(self, name, content, time, planned, completed):
         self.name = name
         self.content = content
         self.time = time
+        self.planned = planned
+        self.completed = completed
