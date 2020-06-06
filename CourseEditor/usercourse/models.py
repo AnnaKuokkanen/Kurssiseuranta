58f084e161ae28f83e0fc14478478caa3c8f4374
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-06-07 02:18:18 +0300

    Lisätään kurssille attributit 'käyty' ja 'suunniteltu'

--- CourseEditor/usercourse/models.py
+++ CourseEditor/usercourse/models.py
@@ -1,7 +1,18 @@
 from CourseEditor import db
-from sqlalchemy import Table, Integer, ForeignKey, Column
+#from sqlalchemy import Table, Integer, ForeignKey, Column
 
-UserCourse = db.Table('account_course',
-                        db.Column('user_id', db.Integer, db.ForeignKey('account.id', ondelete='cascade'), primary_key=True), 
-                        db.Column('course_id', db.Integer, db.ForeignKey('course.id', ondelete='cascade'), primary_key=True))
+class UserCourse(db.Model):
+    __tablename__ = "account_course"
+    user_id = db.Column(db.Integer, db.ForeignKey('account.id', ondelete='cascade'), primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id', ondelete='cascade'), primary_key=True)
+    completed = db.Column(db.Boolean, nullable=False)
+    planned = db.Column(db.Boolean, nullable=False)
+
+    def __init__(self, completed, planned):
+        self.completed = False
+        self.planned = False
+
+#UserCourse = db.Table('account_course',
+                        #db.Column('user_id', db.Integer, db.ForeignKey('account.id', ondelete='cascade'), primary_key=True), 
+                        #db.Column('course_id', db.Integer, db.ForeignKey('course.id', ondelete='cascade'), primary_key=True))
 
