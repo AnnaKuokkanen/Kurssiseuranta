f8d7a69a2a64619cd0386c1ff7cb4d328950dfb4
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-06-08 00:52:47 +0300

    Näytetään kaikki kurssit arvosana- ja suunnittelunäkymässä

--- CourseEditor/grades/views.py
+++ CourseEditor/grades/views.py
@@ -1,9 +1,11 @@
 from CourseEditor import app
 from CourseEditor.courses.models import Course
+from CourseEditor.usercourse.models import UserCourse
 from flask import render_template
 from flask_login import current_user, login_required
 
 @app.route("/grades/grades.html") 
 @login_required
 def grades_show():
-    return render_template("grades/grades.html", grades = Course.list_course_and_teacher(current_user.id))
+    #return render_template("grades/grades.html", grades = Course.find_completed_courses(current_user.id))
+    return render_template("grades/grades.html", grades = Course.query.all())
--- CourseEditor/plans/views.py
+++ CourseEditor/plans/views.py
@@ -1,9 +1,11 @@
 from CourseEditor import app
 from CourseEditor.courses.models import Course
+from CourseEditor.usercourse.models import UserCourse
 from flask import render_template
 from flask_login import current_user, login_required
 
 @app.route("/plans/plan.html") 
 @login_required
 def plans_show():
-    return render_template("plans/plan.html", plans = Course.list_course_and_teacher(current_user.id))
+    #return render_template("plans/plan.html", plans = Course.find_planned_courses(current_user.id))
+    return render_template("plans/plan.html", plans = Course.query.all())
