8086daf4c252ac8de01d9adf6bb16e6a3315d562
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-06-07 22:13:41 +0300

    Yhteenvetokyselyt, jotka löytävät käyttäjän suoritetut ja suunnitellut kurssit

--- CourseEditor/courses/models.py
+++ CourseEditor/courses/models.py
@@ -58,3 +58,27 @@ class Course(db.Model):
             response.append({"id":row[0]})
         
         return response
+
+    @staticmethod
+    def find_planned_courses(user_id):
+        stmt = text("SELECT Course.id, Course.name, Course.content, Course.time, Teacher.firstname, Teacher.lastname FROM Course, Teacher "
+                    "LEFT JOIN account_course ON user_id = :user AND account_course.planned = :planned").params(user=user_id, planned=True)
+        
+        res = db.engine.execute(stmt)
+        response = []
+        for row in res:
+            response.append({"id":row[0], "name":row[1], "content":row[2], "time":row[3], "firstname":row[4],"lastname":row[5]})
+
+        return response
+
+    @staticmethod
+    def find_completed_courses(user_id):
+        stmt = text("SELECT Course.id, Course.name, Course.content, Course.time, Teacher.firstname, Teacher.lastname FROM Course, Teacher "
+                    "LEFT JOIN account_course ON user_id = :user AND account_course.completed = :completed").params(user=user_id, completed=True)
+        
+        res = db.engine.execute(stmt)
+        response = []
+        for row in res:
+            response.append({"id":row[0], "name":row[1], "content":row[2], "time":row[3], "firstname":row[4],"lastname":row[5]})
+
+        return response
